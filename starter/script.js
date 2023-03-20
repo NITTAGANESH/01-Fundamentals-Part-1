b8d523e26e6bd66396d43bf2d20ba30eb50f01a7
NITTAGANESH <nittaganeshkumar@gmail.com>
2023-03-20 18:31:50 +0530

    js

---
+++ starter/script.js
@@ -0,0 +1,9 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('javascript is FUN!');
+// console.log(40 - 10 + 12)
+
+//variables
+console.log("bob")
+console.log(23)
+let FirstName = "bob1"
+console.log(FirstName)
